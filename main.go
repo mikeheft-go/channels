2a3be4da4e74156b819ec9589e4d3da1bb81288c
Mike Heft <mikeheft@gmail.com>
2024-08-05 07:55:30 -0600

    add loop through channel elements

--- main.go
+++ main.go
@@ -17,17 +17,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		getStatus(link)
+		go getStatus(c, link)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func getStatus(link string) {
+func getStatus(c chan string, link string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println("[ERROR]: ", err)
+		c <- "[ERROR]: Might be down"
 		os.Exit(1)
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "It's up!"
 }
